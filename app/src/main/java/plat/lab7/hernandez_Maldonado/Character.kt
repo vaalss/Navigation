335c4d5955e3f91c220434f9aee14470bdde2472
vaalss <valeria.hernandezmaldo@gmail.com>
2026-09-20 07:40:36 -0600

    Agregar clases base

---
+++ app/src/main/java/plat/lab7/hernandez_Maldonado/Character.kt
@@ -0,0 +1,10 @@
+package plat.lab7.hernandez_Maldonado
+
+data class Character(
+    val id: Int,
+    val name: String,
+    val status: String,
+    val species: String,
+    val gender: String,
+    val image: String
+)
